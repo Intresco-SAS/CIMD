4b9ac98733d6ab368a66d2eabb7a9727b1161944
JuanPinilla198 <87446130+JuanPinilla198@users.noreply.github.com>
2021-11-19 12:12:02 -0500

    commit

--- employee_full_name/models/models.py
+++ employee_full_name/models/models.py
@@ -569,7 +569,7 @@ class EmployeeFullName(models.Model):
         ('widower', 'Widower'),
         ('divorced', 'Divorced'),
         ('union_libre', 'Union Libre'),
-    ], string='Marital Status', default='Single', tracking=True)
+    ], string='Estado civil')
 
     '''def _colombia1(self):
         return self.env['res.country'].search([('code', '=', 'CO')]).id'''
